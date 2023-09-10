8ce95215fe9cfcaed746714a0008d13e6a85f129
dorianpavetic <xyzcod2movies@gmail.com>
2023-09-10 10:59:36 +0200

    Drop files from .gitignore

--- target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE Person;
-CREATE TABLE IF NOT EXISTS Person (
-    oib VARCHAR(11) NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-    status VARCHAR(10) NOT NULL,
-    CONSTRAINT pk_person PRIMARY KEY (oib)
-);
